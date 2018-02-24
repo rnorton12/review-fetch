e57faac7ee539707df19960a41fa4e1748a6465e
Dillon Faulkner <Kist221@Kist221-Mac.local>
2018-02-24 12:09:31 -0700

    updated readme

--- db/schema.sql
+++ db/schema.sql
@@ -1,12 +1,29 @@
 ### Schema
-DROP DATABASE IF EXISTS CHANGE ME;
-CREATE DATABASE CHANGE ME;
-USE CHANGE ME;
+DROP DATABASE IF EXISTS review_fetch;
+CREATE DATABASE review_fetch;
+USE review_fetch;
 
-CREATE TABLE CHANGE ME
-(
+CREATE TABLE fetch_users(
+	id int NOT NULL AUTO_INCREMENT,
+	username varchar(255) NOT NULL,
+	password varchar(255) NOT NULL,
+	bool BOOLEAN DEFAULT false,
+	PRIMARY KEY (id)
+);
+
+CREATE TABLE fetch_company(
 	id int NOT NULL AUTO_INCREMENT,
 	name varchar(255) NOT NULL,
 	bool BOOLEAN DEFAULT false,
+	PRIMARY KEY (id)
+);
+
+-- FEMALE = 0 && MALE = 1
+CREATE TABLE fetch_client_data(
+	id int NOT NULL AUTO_INCREMENT,
+	name varchar(255) NOT NULL,
+	gender BOOLEAN,
+	email varchar(255) NOT NULL,
+	phone varchar(12),
 	PRIMARY KEY (id)
 );
