8dc3ae6dc4cc64be184fcfd300966b1743a92d7d
JamieDavid <jjdavid14@live.com>
2018-03-04 09:03:03 -0700

    be able to now edit contacts

--- controllers/app_controllers.js
+++ controllers/app_controllers.js
@@ -281,14 +281,7 @@ router.post("/api/fetch_contact_data/new", function(req, res) {
 
 //Update contact
 router.post("/api/fetch_contact_data/update", function(req, res) {
-  db.Contact.update({
-    name: req.body.name,
-    gender: req.body.gender,
-    email: req.body.email,
-    phone: req.body.phone,
-    status: req.body.status,
-    active: req.body.active
-  }, {
+  db.Contact.update(req.body, {
     where: {
       id: req.body.id
     }
--- public/assets/js/contact-functions.js
+++ public/assets/js/contact-functions.js
@@ -2,12 +2,12 @@ $(document).ready(function() {
 type = ['','info','success','warning','danger'];
 
 contactFunctions = {
-    showNotification: function(from, align){
+    showNotification: function(from, align, msg){
     var color = 2;
 
     	$.notify({
         	icon: "ti-check",
-        	message: "<b>Contact successfully added!</b>"
+        	message: "<b>" + msg + "</b>"
 
         },{
             type: type[color],
@@ -20,21 +20,98 @@ contactFunctions = {
 	}
 }
 
-var company = $("#new-contact-company");
-var contactlist = $("#new-contact-contactlist");
-var email = $("#new-contact-email");
-var firstname = $("#new-contact-firstname");
-var lastname = $("#new-contact-lastname");
-var gender = $("#new-contact-gender");
-var phone = $("#new-contact-phone");
-
+$(".edit-contact").on("click", editContact)
 $("#new-contact-addbtn").on("click", handleFormSubmit);
+$("#edit-contact-btn").on("click", updateContact);
 
-// A function for handling what happens when the form to create a new post is submitted
+// A function for handling what happens when the form to create a new contact is submitted
   function handleFormSubmit(event) {
     event.preventDefault();
     console.log("Submit!");
+
+    var email = $("#new-contact-email");
+    var firstname = $("#new-contact-firstname");
+    var lastname = $("#new-contact-lastname");
+    var gender = $("#new-contact-gender");
+    var phone = $("#new-contact-phone");
+
+    var newContact = constructContactObject(firstname, lastname, gender, email, phone);
+
+    submitPost(newContact);
+  }
+
+  // This populates the edit modal with the selected contact's details
+  function editContact(event) {
+    var id = (event.target.id).replace('editContact', '');
+    $.get("/api/fetch_contact_data/" + id, function() {
+        console.log("Getting contact with id: " + id);
+    }).done(function(obj) {
+        var name = obj.name.split(' ');
+        $("#edit-contact-email").val(obj.email);
+        $("#edit-contact-firstname").val(name[0]);
+        $("#edit-contact-lastname").val(name[1]);
+        $("#edit-contact-phone").val(obj.phone);
+
+        if(obj.gender == 1) {
+            $("#edit-contact-gender select").val("male");
+        } else {
+            $("#edit-contact-gender select").val("female");
+        }
+
+        if(obj.active == true) {
+            $("#activeCheckbox").checkbox("check");
+        } else {
+            $("#activeCheckbox").checkbox("uncheck");
+        }
+
+        if(obj.status == 0) {
+            $('#edit-contact-status option[value=0]').prop('selected',true);
+        } else if(obj.status == 1) {
+             $('#edit-contact-status option[value=1]').prop('selected',true);
+        } else {
+            $('#edit-contact-status option[value=2]').prop('selected',true);
+        }
+
+        // Give the edit modal the id of the contact through data-id
+        $("#editContactModal").attr("data-id", id);
+    });
+  }
+
+  // A function for handling what happens when the form to save changes on a contact is submitted
+  function updateContact(event) {
+    var editEmail = $("#edit-contact-email");
+    var editFirstname = $("#edit-contact-firstname");
+    var editLastname = $("#edit-contact-lastname");
+    var editGender = $("#edit-contact-gender");
+    var editPhone = $("#edit-contact-phone");
+
+    var contact = constructContactObject(editFirstname, editLastname, editGender, editEmail, editPhone);
+
+    var activeCheckbox = $("#activeCheckbox").prop("checked");
+    contact.active = activeCheckbox;
+    contact.status = $("#edit-contact-status").val();
+    contact.id = $("#editContactModal").attr("data-id");
+
+    $.post("/api/fetch_contact_data/update", contact, function() {
+        var editSuccessMsg = "Changes to Contact Saved!";
+        contactFunctions.showNotification('top','center', editSuccessMsg);
+        setTimeout(function(){ window.location.href='/contacts'; }, 2000);
+    });
+  }
+
+  /* This will make an object from the arguments passed in
+   * @return {
+   *             name: firstname + ' ' + lastname,
+   *             gender: gender,
+   *             email: email,
+   *             phone: phone,
+   *             companyId: 1
+   *         } 
+  */
+  function constructContactObject(firstname, lastname, gender, email, phone) {
+    // Combine the firstname and lastname
     var fullName = firstname.val().trim() + " " + lastname.val().trim();
+    // Assigns 1 for male and 0 for female (stored as boolean)
     if(gender.val() === "male") {
         gender = 1;
     } else if(gender.val() === "female") {
@@ -43,8 +120,8 @@ $("#new-contact-addbtn").on("click", handleFormSubmit);
         gender = 1;
     }
 
-    // Constructing a newContact object to hand to the database
-    var newContact = {
+    // Make the object
+    var contact = {
         name: fullName
             .trim(),
         gender: gender,
@@ -57,15 +134,17 @@ $("#new-contact-addbtn").on("click", handleFormSubmit);
         CompanyId: 1
     };
 
-    submitPost(newContact);
+    // Return the object
+    return contact;
   }
 
   // Submits a new contact and shows a notification upon completion
   function submitPost(contact) {
     console.log("Posting");
     $.post("/api/fetch_contact_data/new", contact, function() {
-        contactFunctions.showNotification('top','center');
-        setTimeout(function(){ window.location.href='/contacts'; }, 5000);
+        var successAddMsg = "Contact Added Successfully!";
+        contactFunctions.showNotification('top','center', successAddMsg);
+        setTimeout(function(){ window.location.href='/contacts'; }, 2000);
     });
   }
   });
