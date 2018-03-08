8ba2ab7cc4b5c0f6c76f6cf74345b1efcad06d5a
JamieDavid <jjdavid14@live.com>
2018-03-08 00:49:49 -0700

    remodel our tables sequelize models to incorporate passport.js

--- models/users.js
+++ models/users.js
@@ -1,3 +1,6 @@
+// Requiring bcrypt for password hashing. Using the bcrypt-nodejs version as the regular bcrypt module
+var bcrypt = require("bcrypt-nodejs");
+// Creating our Users model
 module.exports = function(sequelize, DataTypes) {
   var Users = sequelize.define("Users", {
     // Giving the Users model a username of type STRING
@@ -20,6 +23,7 @@ module.exports = function(sequelize, DataTypes) {
     email: {
       type: DataTypes.STRING,
       allowNull: false,
+      unique: true,
       validate: {
         isEmail: true,
         len: [1]
@@ -42,6 +46,16 @@ module.exports = function(sequelize, DataTypes) {
       onDelete: "cascade"
     });
   };
+
+  // Creating a custom method for our User model. This will check if an unhashed password entered by the user can be compared to the hashed password stored in our database
+  Users.prototype.validPassword = function(password) {
+    return bcrypt.compareSync(password, this.password);
+  };
+  // Hooks are automatic methods that run during various phases of the User Model lifecycle
+  // In this case, before a User is created, we will automatically hash their password
+  Users.hook("beforeCreate", function(user) {
+    user.password = bcrypt.hashSync(user.password, bcrypt.genSaltSync(10), null);
+  });
 
   return Users;
 };
